ac3f81dc40d0cc5c24fa45b913b257e306a4b505
Restyled.io <commits@restyled.io>
2020-06-09 07:37:10 +0000

    Restyled by black

--- libretro-common/glsym/glgen.py
+++ libretro-common/glsym/glgen.py
@@ -25,7 +25,34 @@
 import sys
 import os
 import re
-banned_ext = ['AMD', 'APPLE', 'NV', 'NVX', 'ATI', '3DLABS', 'SUN', 'SGI', 'SGIX', 'SGIS', 'INTEL', '3DFX', 'IBM', 'MESA', 'GREMEDY', 'OML', 'PGI', 'I3D', 'INGL', 'MTX', 'QCOM','IMG', 'ANGLE', 'SUNX', 'INGR' ]
+
+banned_ext = [
+    "AMD",
+    "APPLE",
+    "NV",
+    "NVX",
+    "ATI",
+    "3DLABS",
+    "SUN",
+    "SGI",
+    "SGIX",
+    "SGIS",
+    "INTEL",
+    "3DFX",
+    "IBM",
+    "MESA",
+    "GREMEDY",
+    "OML",
+    "PGI",
+    "I3D",
+    "INGL",
+    "MTX",
+    "QCOM",
+    "IMG",
+    "ANGLE",
+    "SUNX",
+    "INGR",
+]
 
 
 def noext(sym):
@@ -36,14 +63,17 @@ def fix_multiline_functions(lines):
     fixed_lines = []
     temp_lines = []
     for line in lines:
-        if line.count('(') > line.count(')'):
+        if line.count("(") > line.count(")"):
             temp_lines.append(line)
         else:
             if temp_lines:
-                if line.count(')') > line.count('('):
+                if line.count(")") > line.count("("):
                     temp_lines.append(line)
                     fixed_line = re.sub(
-                        ' +', ' ', ''.join(temp_lines).replace('\n', '').replace('\t', ''))
+                        " +",
+                        " ",
+                        "".join(temp_lines).replace("\n", "").replace("\t", ""),
+                    )
                     fixed_lines.append(fixed_line)
                     temp_lines = []
                 else:
@@ -57,105 +87,116 @@ def find_gl_symbols(lines):
     typedefs = []
     syms = []
     for line in lines:
-        m = re.search(r'^typedef.+PFN(\S+)PROC.+$', line)
-        g = re.search(r'^.+(gl\S+)\W*\(.+\).*$', line)
+        m = re.search(r"^typedef.+PFN(\S+)PROC.+$", line)
+        g = re.search(r"^.+(gl\S+)\W*\(.+\).*$", line)
         if m and noext(m.group(1)):
-            typedefs.append(m.group(0).replace('PFN', 'RGLSYM').replace(
-                'GLDEBUGPROC', 'RGLGENGLDEBUGPROC'))
+            typedefs.append(
+                m.group(0)
+                .replace("PFN", "RGLSYM")
+                .replace("GLDEBUGPROC", "RGLGENGLDEBUGPROC")
+            )
         if g and noext(g.group(1)):
             syms.append(g.group(1))
     return (typedefs, syms)
 
 
 def generate_defines(gl_syms):
-    return ['#define {} __rglgen_{}'.format(line, line) for line in gl_syms]
+    return ["#define {} __rglgen_{}".format(line, line) for line in gl_syms]
 
 
 def generate_declarations(gl_syms):
-    return ['RGLSYM' + x.upper() + 'PROC ' + '__rglgen_' + x + ';' for x in gl_syms]
+    return ["RGLSYM" + x.upper() + "PROC " + "__rglgen_" + x + ";" for x in gl_syms]
 
 
 def generate_macros(gl_syms):
-    return ['    SYM(' + x.replace('gl', '') + '),' for x in gl_syms]
+    return ["    SYM(" + x.replace("gl", "") + ")," for x in gl_syms]
 
 
 def dump(f, lines):
-    f.write('\n'.join(lines))
-    f.write('\n\n')
+    f.write("\n".join(lines))
+    f.write("\n\n")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
 
     if len(sys.argv) > 4:
         for banned in sys.argv[4:]:
             banned_ext.append(banned)
 
-    with open(sys.argv[1], 'r') as f:
+    with open(sys.argv[1], "r") as f:
         lines = fix_multiline_functions(f.readlines())
         typedefs, syms = find_gl_symbols(lines)
 
         overrides = generate_defines(syms)
         declarations = generate_declarations(syms)
-        externs = ['extern ' + x for x in declarations]
+        externs = ["extern " + x for x in declarations]
 
         macros = generate_macros(syms)
 
-    with open(sys.argv[2], 'w') as f:
-        f.write('#ifndef RGLGEN_DECL_H__\n')
-        f.write('#define RGLGEN_DECL_H__\n')
+    with open(sys.argv[2], "w") as f:
+        f.write("#ifndef RGLGEN_DECL_H__\n")
+        f.write("#define RGLGEN_DECL_H__\n")
 
-        f.write('#ifdef __cplusplus\n')
+        f.write("#ifdef __cplusplus\n")
         f.write('extern "C" {\n')
-        f.write('#endif\n')
+        f.write("#endif\n")
 
-        f.write('#ifdef GL_APIENTRY\n')
-        f.write('typedef void (GL_APIENTRY *RGLGENGLDEBUGPROC)(GLenum, GLenum, GLuint, GLenum, GLsizei, const GLchar*, GLvoid*);\n')
-        f.write('typedef void (GL_APIENTRY *RGLGENGLDEBUGPROCKHR)(GLenum, GLenum, GLuint, GLenum, GLsizei, const GLchar*, GLvoid*);\n')
-        f.write('#else\n')
-        f.write('#ifndef APIENTRY\n')
-        f.write('#define APIENTRY\n')
-        f.write('#endif\n')
-        f.write('#ifndef APIENTRYP\n')
-        f.write('#define APIENTRYP APIENTRY *\n')
-        f.write('#endif\n')
-        f.write('typedef void (APIENTRY *RGLGENGLDEBUGPROCARB)(GLenum, GLenum, GLuint, GLenum, GLsizei, const GLchar*, GLvoid*);\n')
-        f.write('typedef void (APIENTRY *RGLGENGLDEBUGPROC)(GLenum, GLenum, GLuint, GLenum, GLsizei, const GLchar*, GLvoid*);\n')
-        f.write('#endif\n')
+        f.write("#ifdef GL_APIENTRY\n")
+        f.write(
+            "typedef void (GL_APIENTRY *RGLGENGLDEBUGPROC)(GLenum, GLenum, GLuint, GLenum, GLsizei, const GLchar*, GLvoid*);\n"
+        )
+        f.write(
+            "typedef void (GL_APIENTRY *RGLGENGLDEBUGPROCKHR)(GLenum, GLenum, GLuint, GLenum, GLsizei, const GLchar*, GLvoid*);\n"
+        )
+        f.write("#else\n")
+        f.write("#ifndef APIENTRY\n")
+        f.write("#define APIENTRY\n")
+        f.write("#endif\n")
+        f.write("#ifndef APIENTRYP\n")
+        f.write("#define APIENTRYP APIENTRY *\n")
+        f.write("#endif\n")
+        f.write(
+            "typedef void (APIENTRY *RGLGENGLDEBUGPROCARB)(GLenum, GLenum, GLuint, GLenum, GLsizei, const GLchar*, GLvoid*);\n"
+        )
+        f.write(
+            "typedef void (APIENTRY *RGLGENGLDEBUGPROC)(GLenum, GLenum, GLuint, GLenum, GLsizei, const GLchar*, GLvoid*);\n"
+        )
+        f.write("#endif\n")
 
-        f.write('#ifndef GL_OES_EGL_image\n')
-        f.write('typedef void *GLeglImageOES;\n')
-        f.write('#endif\n')
+        f.write("#ifndef GL_OES_EGL_image\n")
+        f.write("typedef void *GLeglImageOES;\n")
+        f.write("#endif\n")
 
-        f.write('#if !defined(GL_OES_fixed_point) && !defined(HAVE_OPENGLES2)\n')
-        f.write('typedef GLint GLfixed;\n')
-        f.write('#endif\n')
+        f.write("#if !defined(GL_OES_fixed_point) && !defined(HAVE_OPENGLES2)\n")
+        f.write("typedef GLint GLfixed;\n")
+        f.write("#endif\n")
 
-        f.write('#if defined(OSX) && !defined(MAC_OS_X_VERSION_10_7)\n')
-        f.write('typedef long long int GLint64;\n')
-        f.write('typedef unsigned long long int GLuint64;\n')
-        f.write('typedef unsigned long long int GLuint64EXT;\n')
-        f.write('typedef struct __GLsync *GLsync;\n')
-        f.write('#endif\n')
+        f.write("#if defined(OSX) && !defined(MAC_OS_X_VERSION_10_7)\n")
+        f.write("typedef long long int GLint64;\n")
+        f.write("typedef unsigned long long int GLuint64;\n")
+        f.write("typedef unsigned long long int GLuint64EXT;\n")
+        f.write("typedef struct __GLsync *GLsync;\n")
+        f.write("#endif\n")
 
         dump(f, typedefs)
         dump(f, overrides)
         dump(f, externs)
 
-        f.write('struct rglgen_sym_map { const char *sym; void *ptr; };\n')
-        f.write('extern const struct rglgen_sym_map rglgen_symbol_map[];\n')
+        f.write("struct rglgen_sym_map { const char *sym; void *ptr; };\n")
+        f.write("extern const struct rglgen_sym_map rglgen_symbol_map[];\n")
 
-        f.write('#ifdef __cplusplus\n')
-        f.write('}\n')
-        f.write('#endif\n')
+        f.write("#ifdef __cplusplus\n")
+        f.write("}\n")
+        f.write("#endif\n")
 
-        f.write('#endif\n')
+        f.write("#endif\n")
 
-    with open(sys.argv[3], 'w') as f:
+    with open(sys.argv[3], "w") as f:
         f.write('#include "glsym/glsym.h"\n')
-        f.write('#include <stddef.h>\n')
+        f.write("#include <stddef.h>\n")
         f.write('#define SYM(x) { "gl" #x, &(gl##x) }\n')
-        f.write('const struct rglgen_sym_map rglgen_symbol_map[] = {\n')
+        f.write("const struct rglgen_sym_map rglgen_symbol_map[] = {\n")
         dump(f, macros)
-        f.write('    { NULL, NULL },\n')
-        f.write('};\n')
+        f.write("    { NULL, NULL },\n")
+        f.write("};\n")
         dump(f, declarations)
